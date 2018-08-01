fec4374b6122bac350b22fa40e9fba4bb464325b
Alice Rose <alice@acolorbright.com>
2018-08-01 18:43:59 +0200

    Update rules.

--- rules/best-practices.js
+++ rules/best-practices.js
@@ -2,6 +2,8 @@ module.exports = {
   rules: {
     'class-methods-use-this': 0,
     'consistent-return': 0,
+    'no-else-return': 1,
     'no-param-reassign': ['error', { props: false }],
+    'operator-linebreak': 1,
   },
 };
